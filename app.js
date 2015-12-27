11969904cce593a726ce9cad8d152d88d73693ec
unbuntu server <hxh@localhost.(none)>
2015-12-27 10:43:57 -0800

    add home page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var koa = require('koa'),
     app = module.exports = koa(),
     url = require('url'),
     router = require('koa-route'),
+    views = require('co-views'),
     ga = require('./lib/generateAvatar');
 
 // custom 401 handling
@@ -24,17 +25,23 @@ app.use(function* (next){
 
 // secret response
 
+var render= views(__dirname + '/views', { map: { html: 'swig' }});
 
 
 
-
-var avatar =  function *(name){
+function* avatar(name){ 
   var parseUrl = url.parse(decodeURI(this.request.url),true);
-  var filename = yield ga(name,parseUrl.query.size);
+  var filename = yield ga(name.toString(),parseUrl.query.size);
   this.type = 'image/png';
   this.body = yield fs.readFile(filename);
 }
 
+
+function* index(){
+  this.body = yield render('index.jade', { });
+}
+
+app.use(router.get('/', index));
 app.use(router.get('/avatar', avatar));
 app.use(router.get('/avatar/:name', avatar));
 
